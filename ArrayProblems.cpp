58f33a3651b67ed22c1dd0aa32eecc4450bdcc49
Yuvraj Varshney <125182825+Yuvraj2606@users.noreply.github.com>
2025-03-18 16:59:57 +0530

    Update ArrayProblems.cpp

--- ArrayProblems.cpp
+++ ArrayProblems.cpp
@@ -85,7 +85,7 @@
 // #include <iostream>
 // using namespace std;
 // void merge(int arr1[], int m, int arr2[], int n, int *arr3)
-// { // arr1[] = {1,3,5,7} 3 arr2[] = {2,4,6} 3  k=1 arr3={1,2,3,4,5,6
+// { // arr1[] = {1,3,5,7} 3 arr2[] = {2,4,6} 3  k=1 arr3={1,2,3,4,5,6}
 //     int k = 0;
 //     int i = 0, j = 0;
 //     while (i < m && j < n)
